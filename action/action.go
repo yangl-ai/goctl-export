0d96147fb5b9bcde0f8239e7fba85fe6d5570d4c
yang.liang <ylother@163.com>
2021-08-15 21:09:57 +0800

    add spew dump

--- action/action.go
+++ action/action.go
@@ -2,6 +2,7 @@ package action
 
 import (
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/tal-tech/go-zero/tools/goctl/plugin"
 	"github.com/urfave/cli/v2"
 )
@@ -11,6 +12,7 @@ func Generator(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
+	spew.Dump(p)
 	fmt.Printf("plugin args %+v", p)
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/yangl-ai/goctl-postman
 go 1.15
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/tal-tech/go-zero v1.1.10
 	github.com/urfave/cli/v2 v2.3.0
+	go.uber.org/automaxprocs v1.4.0 // indirect
 )
